b9c8852cb156c0629188177e5d190a9f783c9222
Shiverino <tibodw4@icloud.com>
2023-05-01 15:24:41 +0200

    fix voor auto script te doen werken, nu kan je via input .vdi file meegeven

--- vbox-auto.sh
+++ vbox-auto.sh
@@ -11,8 +11,14 @@ VM_MEMORY="2048"
 VM_CPUS="4"
 
 # Hard drive settings
-HDD_NAME="/Users/sasuke/Downloads/Debian 10 CLI (64bit).vdi"
-HDD_NAME_2="/Users/sasuke/Downloads/Kali-2 Linux 2022.3 (64bit).vdi"
+#VDI_PATH="/Users/sasuke/Downloads/64bit 3/Kali Linux 2022.3 (64bit).vdi"
+#VDI_PATH_2="/Users/sasuke/Downloads/64bit 10/Debian 10 CLI (64bit).vdi"
+
+read -r -p "Enter the path to the vdi file for debian server: " VDI_PATH
+
+read -r -p "Enter the path to the vdi file for kali server: " VDI_PATH_2
+
+
 
 
 # Create the virtual machine
@@ -23,7 +29,7 @@ VBoxManage modifyvm "$VM_NAME" --memory "$VM_MEMORY" --cpus "$VM_CPUS"
 
 # Attach the virtual hard drive to the virtual machine
 VBoxManage storagectl "$VM_NAME" --name "SATA Controller" --add sata --controller IntelAHCI
-VBoxManage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$HDD_NAME"
+VBoxManage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$VDI_PATH"
 
 # Attach the ISO image to the virtual machine
 VBoxManage storagectl "$VM_NAME" --name "IDE Controller" --add ide
@@ -44,7 +50,7 @@ VBoxManage modifyvm "$VM_NAME_2" --memory "$VM_MEMORY" --cpus "$VM_CPUS"
 
 # Attach the virtual hard drive to the virtual machine
 VBoxManage storagectl "$VM_NAME_2" --name "SATA Controller" --add sata --controller IntelAHCI
-VBoxManage storageattach "$VM_NAME_2" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$HDD_NAME_2"
+VBoxManage storageattach "$VM_NAME_2" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$VDI_PATH_2"
 
 # Attach the ISO image to the virtual machine
 VBoxManage storagectl "$VM_NAME_2" --name "IDE Controller" --add ide
